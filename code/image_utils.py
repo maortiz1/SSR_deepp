f1da0f9854a4d2c5bfa9c356192e4e63767a20a0
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-07-16 17:11:34 -0400

    dwn_iso

--- code/image_utils.py
+++ code/image_utils.py
@@ -27,6 +27,61 @@ def normalize_image_whitestripe(img,contrast= 'T1'):
     mask = whitestripe.whitestripe(img,contrast)
     norm_image = whitestripe.whitestripe_norm(img,mask)
     return norm_image
+def downsample_isotropic(img,down_factor=3):
+    import numpy as np
+    import math
+    from skimage.transform import resize
+    from dipy.denoise.nlmeans import nlmeans
+    from dipy.denoise.noise_estimate import estimate_sigma
+    fft_imag = np.fft.fftn(img)#n-fourier transform
+    shift_fft_imag = np.fft.fftshift(fft_imag)#fft shifting(zero-frequency component to the center of the spectrum)
+    z_size= shift_fft_imag.shape[2]#z_dimension size for cropping
+    size = math.floor(z_size/(down_factor))#size of part according to down factor
+    center = round(z_size/2)-1 #center for cutting
+    if size%2==0:
+        crop_ffs = shift_fft_imag[::,::,int(center-(size/2)):int(center+(size/2))]
+    else:
+        size = size+1        
+        crop_ffs = shift_fft_imag[::,::,int(center-(size/2)+1):int(center+(size/2))]
+    #crop_ffs = shift_fft_imag[::,::,center-size:center+size]#cropping kspace
+    shift_ifft_crop_ffs = np.fft.ifftshift(crop_ffs)#inverse shiftt
+    ifft_crop_ffs = np.fft.ifftn(shift_ifft_crop_ffs)#inverse fast fourier transform
+    out = resize(abs(ifft_crop_ffs),output_shape=img.shape,mode='symmetric',order=3)
+    # sigma_esti = estimate_sigma(img, N=0)
+    # import matplotlib.pyplot as plt
+    
+    # out_filter=nlmeans(out, sigma=sigma_esti, patch_radius= 1, block_radius = 1, rician= True)
+
+    # fig,ax = plt.subplots(1,3)
+    # ax[0].imshow(img[::,50,100:150],cmap='gray')    
+    # ax[1].imshow(out[::,50,100:150],cmap='gray')
+    # ax[2].imshow(out_filter[::,50,100:150],cmap='gray')
+
+    # plt.show()
+    return out
+    
+def downsample_croping(img_nib,down_factor=3):
+    import numpy as np
+    import nibabel.processing as pr
+    import math
+    fft_imag = np.fft.fftn(img)#n-fourier transform
+    shift_fft_imag = np.fft.fftshift(fft_imag)#fft shifting(zero-frequency component to the center of the spectrum)
+    z_size= shift_fft_imag.shape[2]#z_dimension size for cropping
+    size = math.floor(z_size/(down_factor))#size of part according to down factor
+    center = round(z_size/2)-1 #center for cutting
+    new_fft_zeros = np.zeros(shift_fft_imag.shape)
+    if size%2==0:
+        new_fft_zeros[::,::,center-size/2:center+size/2] = shift_fft_imag[::,::,center-size/2:center+size/2]
+    else:
+        size = size+1        
+        new_fft_zeros[::,::,int(center-(size/2)+1):int(center+(size/2))] = shift_fft_imag[::,::,int(center-(size/2)+1):int(center+(size/2))]
+    #crop_ffs = shift_fft_imag[::,::,center-size:center+size]#cropping kspace
+    shift_ifft_crop_ffs = np.fft.ifftshift(new_fft_zeros)#inverse shiftt
+    ifft_crop_ffs = np.fft.ifftn(shift_ifft_crop_ffs)#inverse fast fourier transform
+    print('Thickness of slice is now ',down_factor, 'times less that it originally was')
+    return abs(ifft_crop_ffs)#returns cropped version of the origianl image 
+
+    
 
 
 #n number of sized voxels from inserted volume
@@ -105,7 +160,7 @@ def cropall(img,vox_size):
 
 
 
-import nibabel as nib
+# import nibabel as nib
 # import os
 # import numpy as np
 # import matplotlib.pyplot as plt
@@ -144,15 +199,28 @@ import nibabel as nib
 # plt.imshow(vox[10,::,24,::],cmap='gray')
 # plt.show()
 
-# import nibabel as nib
-# import os
-# import matplotlib.pyplot as plt
+import nibabel as nib
+import os
+import matplotlib.pyplot as plt
 
-#import os
-#img = nib.load(os.path.join('images','T1_50.nii'))
-#img = img.get_fdata()
-#im_l,n_x,n_y = cropall(img,(32,32))
-#print(len(im_l))
+img = nib.load(os.path.join('images','T1_50.nii'))
+img = img.get_fdata()
+# im_l,n_x,n_y = cropall(img,(32,32))
+img_d= downsample_isotropic(img,3)
+img_d2= downsample(img,3)
+img_d3= downsample_croping(img,3)
+fig,ax = plt.subplots(3,2)
+ax[0,0].imshow(img[::,50,::],cmap='gray')
+ax[0,1].imshow(img_d[::,50,::],cmap='gray')
+ax[1,0].imshow(img[::,50,::],cmap='gray')
+ax[1,1].imshow(img_d2[::,50,::],cmap='gray')
+ax[2,0].imshow(img[::,50,::],cmap='gray')
+ax[2,1].imshow(img_d3[::,50,::],cmap='gray')
+plt.show()
+
+
+
+
 
 # wh = normalize_image_whitestripe(img)
 # data = wh.get_fdata()

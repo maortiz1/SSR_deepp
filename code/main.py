8b6ba92741e490582d97260a80eb2207171a29dc
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-07-24 11:09:34 -0400

    fx

--- code/main.py
+++ code/main.py
@@ -136,20 +136,24 @@ if __name__=='__main__':
       mode_tr=[]
       down_f=[]
       crop = True
+      vox_size=[]
       if arguments.model == 'ResNET':
         n_resblock = arguments.n_resblock
         out_size = arguments.output_sz
         mode_tr = model.ResNET(n_resblocks=n_resblock,output_size=out_size)
         down_f= image_utils.downsample
+        vox_size = (32,32)
       elif arguments.model == 'ResNetIso':
         n_resblock=arguments.n_resblock
         mode_tr = model.ResNetIso(n_resblocks=n_resblock,res_scale=0.1)
         down_f = image_utils.downsample_isotropic
+        vox_size = (32,32)
       elif arguments.model == 'unet3D':
         mode_tr = unet.Unet3D()
         print('in')
         down_f = image_utils.downsample_isotropic
-        crop = False
+        crop = True
+        vox_size = (64,64)
 
       gpu = int(arguments.cuda)
       torch.cuda.set_device(gpu)
@@ -157,7 +161,7 @@ if __name__=='__main__':
       print(device)
       cuda = torch.cuda.is_available()
 
-      dataprep = train.Data_Preparation(arguments.images,crop=crop,downfunction=down_f)
+      dataprep = train.Data_Preparation(arguments.images,crop=crop,downfunction=down_f,vox_size=vox_size)
       #train dataset
       lr_train_vox = dataprep.lr_pcs_tr
       hr_train_vox = dataprep.hr_pcs_tr
@@ -171,10 +175,11 @@ if __name__=='__main__':
       testDataset = train.Dataset(hr_test,lr_test)
       test_data_loader = data.DataLoader(testDataset,batch_size=bt_size,shuffle=False)
       out_f= '%s_lr_%s_bt_%d_rb_%d'%(arguments.model,str(arguments.l_rate).replace('.','_'),bt_size,n_resblock)
-      
+      from torchsummary import summary
       if cuda:
         mode_tr.to(device)
-      
+      summary(mode_tr,np.expand_dims(lr_train_vox[0],axis=0).shape,device='cpu')
+
 
 
       trainer = train.Trainer(train_data_loader,test_data_loader,cuda,3,mode_tr,float(arguments.l_rate),out_f,device)

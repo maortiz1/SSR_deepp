7e42b2c7b732be4291d772fab64c98aa83614e97
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-07-23 21:37:14 -0400

    ship

--- code/unet.py
+++ code/unet.py
@@ -50,19 +50,32 @@ class Unet3D(nn.Module):
 
     def forward(self,x):
         cb0 = self.cb0(x)
+        print(cb0.shape)
         cb1 = self.cb1(cb0)
+        print(cb1.shape)
         pooling1 = self.pooling1(cb1)
+        print(pooling1.shape)
         cb2 = self.cb2(pooling1)
+        print(cb2.shape)
         pooling2 = self.pooling2(cb2)
+        print(pooling2.shape)
         cb3 = self.cb3(pooling2)
+        print(cb3.shape)
         pooling3 = self.pooling3(cb3)
+        print(pooling3.shape)
         cb4 = self.cb4(pooling3)
+        print(cb4.shape)
         pooling4 = self.pooling4(cb4)
+        print(pooling4.shape)
         cb5 = self.cb5(pooling4)    
-       
+        print(cb5.shape)
+
         eb1 = self.eb1(cb5) 
+        print(eb1.shape)
         eb1 = torch.cat((eb1,cb4),1)
+        print(eb1.shape)
         cb6 = self.cb6(eb1)
+        print(cb6.shape)
 
         eb2 = self.eb2(cb6)
         eb2 = torch.cat((eb2,cb3),1)

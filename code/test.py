ccd73b11893f37860544a6bad126449c7f557abc
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-07-25 14:14:13 -0400

    test

--- code/test.py
+++ code/test.py
@@ -29,7 +29,7 @@ class Test():
         if cuda:
             model.to(device)
             from torchsummary import summary
-            summary(model,input_size=(1,1,256,64,64))
+            summary(model,input_size=(1,256,64,64))
           
         self.model = model
         print(self.model)

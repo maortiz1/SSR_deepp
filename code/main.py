663b20379ece220d0124e6e1fe3a2664401fad9e
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-07-29 15:23:00 -0400

    chagnges tes

--- code/main.py
+++ code/main.py
@@ -293,6 +293,7 @@ if __name__=='__main__':
         data = fa.get_fdata()
         data_in = image_utils.upsample_factor(data,factor=3)
         print(data_in.shape)
+        data_in = np.expand_dims(data_in,axis=0)
         x = torch.from_numpy(np.expand_dims(data_in,axis=0).astype(np.float32)).permute(0,3,1,2)
         if cuda:
           x.to(device)

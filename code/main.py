7a490e53fb12c59cce606cdc99b1450665188f59
maortiz1 <ma.ortiz1@uniandes.edu.co>
2019-08-01 16:14:02 -0400

    fix

--- code/main.py
+++ code/main.py
@@ -454,7 +454,8 @@ if __name__=='__main__':
       sz= data_in_wh.shape
       sz_out = (sz[0],sz[1],sz[2]*int(arguments.factor))
       res = resize(data_in_wh,sz_out,mode='symmetric',order=3)
-      pcs,n_pz_x,n_pz_y = image_utils.cropall(data_in_wh,vox_size=(64,64))
+
+      pcs,n_pz_x,n_pz_y = image_utils.cropall(res,vox_size=(64,64))
       scr=[]
       for img in pcs:
         data_crop = np.expand_dims(img,axis=0)
